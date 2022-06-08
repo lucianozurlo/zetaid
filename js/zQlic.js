9d4d56cb7bee34046d45544bf675be712bbb9d76
Luciano Zurlo <lucianozurlo@iMac.local>
2022-06-08 18:51:09 -0300

    v32

--- js/zQlic.js
+++ js/zQlic.js
@@ -1,7 +1,7 @@
 const sentryNext = document.querySelector('#sentryNext');
 
 new fullpage('#zProject', {
-    anchors: ['Intro', 'AdjustablePower', 'Context', 'Working', 'ExplodedView', 'Pedernal', 'PedernalAction', 'Turntable', 'ZetaID', 'NextProject', 'Sentry'],
+    anchors: ['Intro', 'AdjustablePower', 'Context', 'Working', 'ExplodedView', 'Pedernal', 'PedernalAction', 'Turntable', 'ZetaID', 'NextProject', 'NewProject'],
 
     afterLoad: () => {
         document.querySelectorAll('video').forEach(video => video.currentTime = 0);
@@ -25,16 +25,38 @@ new fullpage('#zProject', {
 
     onLeave: function (origin, destination) {
         let leavingSection = this;
-        /*********** 7 *******/
-        /*  6 >>>  */
-        if (origin.index <= 9 && destination.index == 10) {
-            fullpage_api.setScrollingSpeed(700);
-            sentryNext.currentTime = 0;
+
+        /*********** ExplodedView *******/
+        /*  Working >>> ExplodedView <<< Pedernal  */
+        if (origin.index <= 'Working' && destination.index == 'ExplodedView') {
+            // document.querySelector("body.fp-viewing-ExplodedView div[class*='kursor'].kursor--1").style.transform = "scale(0)translate(-50%, -50%)";
+            // document.querySelector("body.fp-viewing-ExplodedView div[class*='kursor'].kursor--1+.kursorChild").style.transform = "scale(0) translate(-50%, -50%)";
+            console.log('hola')
         }
 
-        /*********** 8 *******/
-        /*  7 >>>  */
-        if (origin.index <= 10 && destination.index == 11) {
+        /*********** NextProject *******/
+        /*  ZetaID >>>  */
+        if (origin.index <= 'ZetaID' && destination.index == 'NextProject') {
+            fullpage_api.setScrollingSpeed(700);
+            fliebenNext.currentTime = 0;
+        }
+
+        /*********** NewProject *******/
+        /*  NextProject >>>  */
+        if (origin.index <= 'NextProject' && destination.index == 'NewProject') {
+            fullpage_api.setScrollingSpeed(100000000);
+            fullpage_api.setAllowScrolling(false);
+            setTimeout(() => {
+                console.log('1 sec delay')
+                window.location.href = 'flieben.html';
+                mouse.style.opacity = .25;
+                clearInterval(timer);
+            }, 1000);
+        }
+
+        /*********** 10 *******/
+        /*  9 >>>  */
+        if (origin.index <= 9 && destination.index == 10) {
             fullpage_api.setScrollingSpeed(100000000);
             fullpage_api.setAllowScrolling(false);
             setTimeout(() => {
