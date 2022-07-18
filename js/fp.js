8d8ccde76f5eb0fe41325df2474995ba414f03ed
Luciano Zurlo <lucianozurlo@iMac.local>
2022-07-17 23:19:12 -0300

    v33

--- js/fp.js
+++ js/fp.js
@@ -1,5 +1,5 @@
 
 //In addition to the extension license you'll 
 //need to acquire a fullPage.js license from https://goo.gl/5x9a22
-licenseKey: 'INSERT YOUR FULLPAGE LICENSE KEY HERE'
+licenseKey: '95F4DECD-C6224B51-AE05D665-29136573'
 parallaxKey: 'INSERT YOUR EXTENSION KEY HERE' //see https://goo.gl/xkUmHS
